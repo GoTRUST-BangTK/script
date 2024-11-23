c1600d4f3c10bf12e54ae426083fbd4d84014481
Administrator <admin@example.com>
2024-11-23 20:50:51 +0700

    update

--- auto_update.py
+++ auto_update.py
@@ -1,31 +1,33 @@
 import time
 import requests
-import subprocess 
+import subprocess
 import winreg
+import task_05.python.config as config
 
-# GITEA_API_URL = "http://gitea.local/api/v1/repos/kimbang/script/tags"
-GITEA_API_URL = "https://github.com/GoTRUST-BangTK/script.git"
-GITEA_REPO_DIR = "script"   
-time_interval=5
-registry_path = r"SOFTWARE\AutoUpgrade"
-last_checked_tag = None
+GIT_KIOSK_TAG_API = config.GIT_KIOSK_TAG_API
+GIT_REPO_DIR = config.GIT_REPO_DIR
+REGISTRY_PATH = config.REGISTRY_PATH
+TIME_INTERVAL = config.TIME_INTERVAL
+LAST_CHECKED_TAG = None
+
 
 def get_registry_tag_value(key_name):
     try:
-        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, registry_path, 0, winreg.KEY_READ) as key:
+        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, REGISTRY_PATH, 0, winreg.KEY_READ) as key:
             tag_value, reg_type = winreg.QueryValueEx(key, key_name)
             # print(f"Tag value found: {tag_value}")
             return tag_value
     except FileNotFoundError:
-        print(f"Registry path '{registry_path}' does not exist.")
+        print(f"Registry path '{REGISTRY_PATH}' does not exist.")
         return None
     except OSError as e:
         print(f"An error occurred while accessing the registry: {e}")
         return None
-    
-def create_or_update_registry_key(tag_value,key_name):
+
+
+def create_or_update_registry_key(tag_value, key_name):
     try:
-        with winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, registry_path) as key:
+        with winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, REGISTRY_PATH) as key:
             winreg.SetValueEx(key, key_name, 0, winreg.REG_SZ, tag_value)
             print("Create config success!")
     except PermissionError:
@@ -33,29 +35,33 @@ def create_or_update_registry_key(tag_value,key_name):
     except Exception as e:
         print(f"Create config ERROR! {e}")
 
-def check_new_tag(): 
+
+def check_new_tag():
     try:
-        response = requests.get(GITEA_API_URL)
+        response = requests.get(GIT_KIOSK_TAG_API)
         response.raise_for_status()  # raise exception
         latest_tag = response.json()[0]['name']
-        last_checked_tag=get_registry_tag_value('tag')
-        # print(f"last_checked_tag: {last_checked_tag} ; latest_tag: {latest_tag}")
+        LAST_CHECKED_TAG = get_registry_tag_value('tag')
+        # print(f"LAST_CHECKED_TAG: {LAST_CHECKED_TAG} ; latest_tag: {latest_tag}")
 
         if latest_tag:
-            if latest_tag != last_checked_tag:
+            if latest_tag != LAST_CHECKED_TAG:
                 create_or_update_registry_key(latest_tag, "tag")
                 print(f"New tag found: {latest_tag}")
-                last_checked_tag = latest_tag  
+                LAST_CHECKED_TAG = latest_tag
                 pull_changes()
-                print(f"================== Current tag: {last_checked_tag} ================== \nWaiting for new tag to be pushed in repo")
-            elif latest_tag == None :
+                print(f"================== Current tag: {
+                      LAST_CHECKED_TAG} ================== \nWaiting for new tag to be pushed in repo")
+            elif latest_tag == None:
                 print("No new tag found.")
-                create_or_update_registry_key(latest_tag) 
-                print(f"================== Current tag: {last_checked_tag} ================== \nWaiting for new tag to be pushed in repo")
+                create_or_update_registry_key(latest_tag)
+                print(f"================== Current tag: {
+                      LAST_CHECKED_TAG} ================== \nWaiting for new tag to be pushed in repo")
         else:
             print("No tags found in the repository.")
     except requests.exceptions.RequestException as e:
         print(f"Error checking new tags: {e}")
+
 
 def pull_changes():
     try:
@@ -65,12 +71,15 @@ def pull_changes():
     except subprocess.CalledProcessError as e:
         print(f"Error while pulling changes: {e}")
 
+
 def main():
     print("Start auto-checking for updates from the repo.")
-    print(f"================== Current tag: {last_checked_tag} ================== \nWaiting for new tag to be pushed in repo")
+    print(f"================== Current tag: {
+          LAST_CHECKED_TAG} ================== \nWaiting for new tag to be pushed in repo")
     while True:
-        check_new_tag() 
-        time.sleep(time_interval)
+        check_new_tag()
+        time.sleep(TIME_INTERVAL)
+
 
 if __name__ == "__main__":
     main()
