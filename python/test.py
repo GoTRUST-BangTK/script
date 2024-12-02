7dbde8ef3fb2968e89d563c5bfaa3209b1ccd6ee
Administrator <admin@example.com>
2024-12-02 22:19:56 +0700

    ...

--- python/test.py
+++
@@ -1,6 +0,0 @@
-import config
-import os
-medipay_updater_bin_path = config.MEDIPAY_UPDATER_FOLDER_PATH / "AutoUpgradeApp.exe"
-
-
-print (medipay_updater_bin_path   if os.path.exists(medipay_updater_bin_path) else "")
