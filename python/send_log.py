f42b3d8c23094a694c8d2fb0d126112762bbcb7f
Administrator <admin@example.com>
2024-12-09 10:27:33 +0700

    ...

--- python/send_log.py
+++ python/send_log.py
@@ -1,17 +1,43 @@
-import requests
+import os
+from datetime import datetime
 import config
+import requests
 
-INSTALL_APP_LOG_FILE_PATH = 'd:/sessions.json'
+API_LOG_FOLDER_PATH = r"d:\log"  # config.API_LOG_FOLDER_PATH
 TELEGRAM_API = config.TELEGRAM_API
+
+
+def get_files_created_today(directory):
+    today = datetime.now().date()
+    file_list = []
+
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            try:
+                created_time = datetime.fromtimestamp(
+                    os.path.getctime(file_path)
+                ).date()
+                if created_time == today:
+                    file_list.append(file_path)
+            except OSError as e:
+                print(f"can't get file info {file_path}: {e}")
+    return file_list
+
 
 def send_log(file_path):
     files = {
-        'chat_id': (None, '-4583989930'),
-        'document': open(file_path, 'rb'),
+        "chat_id": (None, "-4583989930"),
+        "document": open(file_path, "rb"),
     }
-    response = requests.post(TELEGRAM_API,
-                             files=files,
-                             )
+    response = requests.post(
+        TELEGRAM_API,
+        files=files,
+    )
     print("Send log to telegram: ", response.status_code, response.text)
 
-send_log(INSTALL_APP_LOG_FILE_PATH)
+
+# ? Loop through each file and print out the path
+for file_path in get_files_created_today(API_LOG_FOLDER_PATH):
+    print(file_path)
+    send_log(file_path)
