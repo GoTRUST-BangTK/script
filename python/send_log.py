94117d5076ccb693b3411ef71defd98ca383c503
Administrator <admin@example.com>
2024-12-22 16:04:15 +0700

    ...

--- python/send_log.py
+++ python/send_log.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import config
 # import requests
 import pysftp
+import logger as setup_logger
 
 # TELEGRAM_API_SEND_FILE = config.TELEGRAM_API_SEND_FILE
 # TELEGRAM_CHAT_ID=config.TELEGRAM_CHAT_ID
@@ -12,6 +13,7 @@ SFTP_HOST = config.SFTP_HOST
 SFTP_PORT = config.SFTP_PORT
 SFTP_USERNAME = config.SFTP_USERNAME
 SFTP_PASSWORD = config.SFTP_PASSWORD
+logger = setup_logger.setup_logger(r"c:\Task_send_log.log")
 
 # def get_files_created_today(directory):
 #     today = datetime.now().date()
@@ -28,6 +30,7 @@ SFTP_PASSWORD = config.SFTP_PASSWORD
 #                     file_list.append(file_path)
 #             except OSError as e:
 #                 print(f"can't get file info {file_path}: {e}")
+#                 logger.info(f"can't get file info {file_path}: {e}")
 #     return file_list
 
 
@@ -41,16 +44,20 @@ SFTP_PASSWORD = config.SFTP_PASSWORD
 #         files=files,
 #     )
 #     print("Send log to telegram: ", response.status_code, response.text)
+#     logger.info("Send log to telegram: ", response.status_code, response.text)
 
 # # # ? Loop through each file and print out the path
+# # # ? Loop through each file and logger.info out the path
 # # for file_path in get_files_created_today(API_LOG_FOLDER_PATH):
 # #     print(file_path)
+# #     logger.info(file_path)
 # #     send_log(file_path)
  
 def send_log_sftp():
     # check existence
     if not os.path.exists(API_LOG_FOLDER_PATH):
         print(f"Log folder {API_LOG_FOLDER_PATH} does not exist.")
+        logger.info(f"Log folder {API_LOG_FOLDER_PATH} does not exist.")
         return
 
     # Create connection 
@@ -60,12 +67,14 @@ def send_log_sftp():
     try:
         with pysftp.Connection(host=SFTP_HOST, port=SFTP_PORT, username=SFTP_USERNAME, password=SFTP_PASSWORD, cnopts=cnopts) as sftp:
             print("Connected to SFTP server.")
+            logger.info("Connected to SFTP server.")
 
             # create folder in SFTP server
             remote_log_folder = f"/Log/kioskId"
             if not sftp.exists(remote_log_folder):
                 sftp.makedirs(remote_log_folder)
                 print(f"Created remote directory: {remote_log_folder}")
+                logger.info(f"Created remote directory: {remote_log_folder}")
 
             # Loop through each file in the local folder and send it to SFTP
             for filename in os.listdir(API_LOG_FOLDER_PATH):
@@ -77,13 +86,17 @@ def send_log_sftp():
                         # check existence in server, if not, upload
                         if sftp.exists(remote_file_path):
                             print(f"File {filename} already exists on SFTP server, skipping upload.")
+                            logger.info(f"File {filename} already exists on SFTP server, skipping upload.")
                         else:
                             # Upload file to SFTP server
                             sftp.put(file_path, remote_file_path)
                             print(f"Uploaded: {filename} to {remote_file_path}")
+                            logger.info(f"Uploaded: {filename} to {remote_file_path}")
                     except Exception as e:
                         print(f"Failed to upload {filename}: {e}")
+                        logger.error(f"Failed to upload {filename}: {e}")
     except Exception as e:
         print(f"Failed to connect to SFTP server: {e}")
+        logger.error(f"Failed to connect to SFTP server: {e}")
 
 send_log_sftp()
